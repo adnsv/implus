3c287bc00430bff480610942f0e9421fd710f893
Alex Denisov <alex.a.denisov@gmail.com>
2024-09-05 00:52:28 -0400

    added simplified simbox api for rendering icd views

--- include/implus/selbox.hpp
+++ include/implus/selbox.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
-#include "implus/content.hpp"
-#include "implus/id.hpp"
-#include "implus/interact.hpp"
 #include <functional>
 #include <imgui.h>
+#include <implus/blocks.hpp>
+#include <implus/content.hpp>
+#include <implus/icd.hpp>
+#include <implus/id.hpp>
+#include <implus/interact.hpp>
 #include <string_view>
 
 const auto ImGuiSelectableFlags_ExtendFrameHorz = ImGuiSelectableFlags(1 << 29);
@@ -14,7 +16,34 @@ namespace ImPlus {
 
 // note: name is used only for test_engine
 auto SelectableBox(ImID id, char const* name, bool selected, ImGuiSelectableFlags flags,
-    ImVec2 size, InteractColorSetCallback on_color, Content::DrawCallback draw_callback)
-    -> InteractState;
+    ImVec2 size, InteractColorSetCallback on_color,
+    Content::DrawCallback draw_callback) -> InteractState;
+
+inline auto Selectable(ImID id, char const* name, bool selected, ImGuiSelectableFlags flags,
+    ImPlus::IconBlock const& v) -> InteractState
+{
+    return SelectableBox(id, name, selected, flags, v.Size, {},
+        [&](ImDrawList* dl, ImVec2 const& tl, ImVec2 const& br, ImPlus::ColorSet const& cs) {
+            v.RenderXY(dl, tl, cs.Content);
+        });
+}
+
+inline auto Selectable(ImID id, char const* name, bool selected, ImGuiSelectableFlags flags,
+    ImPlus::TextBlock const& v) -> InteractState
+{
+    return SelectableBox(id, name, selected, flags, v.Size, {},
+        [&](ImDrawList* dl, ImVec2 const& tl, ImVec2 const& br, ImPlus::ColorSet const& cs) {
+            v.Render(dl, tl, br, cs.Content);
+        });
+}
+
+inline auto Selectable(ImID id, char const* name, bool selected, ImGuiSelectableFlags flags,
+    ImPlus::ICDBlock const& v) -> InteractState
+{
+    return SelectableBox(id, name, selected, flags, v.Size, {},
+        [&](ImDrawList* dl, ImVec2 const& tl, ImVec2 const& br, ImPlus::ColorSet const& cs) {
+            v.Render(dl, tl, br, cs.Content);
+        });
+}
 
 } // namespace ImPlus
